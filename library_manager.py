2cf841f6e367e1e52ed43a3f30588ba38b499eb6
Kishor-Kumar47 <165241741+Kishor-Kumar47@users.noreply.github.com>
2025-03-19 10:43:01 +0500

    library_manager.py

--- library_manager.py
+++ library_manager.py
@@ -186,7 +186,7 @@ def search_book(search_term, search_by):
 # caculate library stats
 def get_library_stats():
     total_books = len(st.session_state.library)
-    read_books = sum(1 for book in st.session_state.library if book['read status'])
+    read_books = sum(1 for book in st.session_state.library if book['read_status'])
     percent_read = (read_books / total_books * 100) if total_books > 0 else 0
 
     genres = {}
